dddfae7b0c73afe6fa8fba7dd836e33877963841
Ferian Satria <148834456+ferian-satria@users.noreply.github.com>
2024-01-28 10:38:56 +0700

    First lesson from Darts
    Strings are part of the Fundamental Dart

---
+++ strings.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('"What do you think of Dart?" he asked');
+  print('"I think it\s great!" I answered confidently');
+  var name = 'Ferian' ;
+  print('Hello $name, nice to meet you.');
+  print('1+1= ${1+1}');
+  print(r'Dia baru saja membeli komputer seharga $1,000.00');
+  print('Hi \u2665');
+}
